ac6e332c5ddc3953e73cd71805d636a1ce810a41
todd arbetter <todd.e.arbetter@gmail.com>
2025-07-10 10:53:33 -0600

    Update hello_world.py
    added 3 lines to output

--- hello_world.py
+++ hello_world.py
@@ -7,5 +7,10 @@ def print_string(string):
     return
 
 string = 'hello world'
+string2 = ''
+string3 = 'it''s Todd'
 
 print_string(string)
+print_string(string2)
+print_string(string3)
+print_string(string2)
